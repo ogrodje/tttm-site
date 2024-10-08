065c2cdaaed2ccd4b4cead6a25bef6806dce321b
Oto Brglez <otobrglez@gmail.com>
2024-10-08 12:49:57 +0200

    Small fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+    integrations: [mdx()],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    silenceDeprecations: ["legacy-js-api"],
+                },
+            },
+        },
+    },
+    devToolbar: {
+        enabled: false,
+    },
 });
